48ae4e507a64ef94b7dd4cf0fd59ae88bf5aae4e
Alexander Clark <alexanderthclark@users.noreply.github.com>
2021-11-28 14:28:16 -0500

    Add files via upload

---
+++ Python/Prose/chart.py
@@ -0,0 +1,13 @@
+x = np.linspace(0,1,2) 
+plt.plot(x,x)
+plt.title("My Chart")
+
+ax = plt.gca()
+print(ax.title)
+
+ax.plot(x, 1 - x)
+ax.set_title('My Wholesome Chart')
+print(ax.title)
+
+fig = plt.gcf()
+fig.savefig('chart.pdf') # same as plt.savefig
