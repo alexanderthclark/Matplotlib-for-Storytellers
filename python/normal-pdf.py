6352b0c658d30670adbc2146b567c84d4b351204
Alexander Clark <alexanderthclark@gmail.com>
2023-07-15 19:07:17 -0400

    stats cleanup

---
+++ python/normal-pdf.py
@@ -0,0 +1,18 @@
+x = np.linspace(-4, 4, 100_000)
+y = stats.norm.pdf(x)
+fig, ax = plt.figure(), plt.axes()
+ax.set_aspect(5)
+
+ax.plot(x,y, linewidth = 2, color = 'C0')
+ax.fill_between(x,y, color = 'C0')
+
+ax.set_yticks([])
+for s in 'left', 'top', 'right':
+    ax.spines[s].set_visible(False)
+    
+ylims = ax.get_ylim()
+ax.set_ylim(0, ylims[1])
+ax.set_title("Normal Distribution")
+#truncate
+plt.tight_layout()
+plt.savefig("normal-pdf.pdf")
