1b2391a611bf081e1aea9bb7ec054fcd35deb144
Alexander Clark <alexanderthclark@users.noreply.github.com>
2026-09-17 16:43:24 -0400

    Complete multidimensional scaling chapter

--- python/mds-circles.py
+++ python/mds-circles.py
@@ -1,35 +1,28 @@
-white = colorConverter.to_rgba('white', alpha = 0)
-grey = colorConverter.to_rgba('gray', alpha = 0.8)
+transparent = (1, 1, 1, 0)
+grey = (.5, .5, .5, .8)
 
-fig, ax = plt.figure(), plt.axes()
+fig, ax = plt.subplots()
 
-# point a
-c = plt.Circle((0, 0), radius = 1, facecolor = white, edgecolor = 'black', alpha = 1)
-ax.add_artist(c)
-ax.plot([0],[0], marker = 'o', markersize = 3)
-ax.text(0,0,'$a$', va = 'bottom', ha = 'left')
+points = {'a': (0, 0),
+          'b': (0, 1),
+          'c': (np.sqrt(3)/2, .5)}
 
-# point b
-c = plt.Circle((0, 1), radius = 1, facecolor = white, edgecolor = grey)
-ax.add_artist(c)
-ax.plot([0],[1], marker = 'o', markersize = 3)
-ax.text(0,1,'$b$', va = 'bottom', ha = 'left')
+for label, point in points.items():
+    edgecolor = 'black' if label == 'a' else grey
+    circle = plt.Circle(point,
+                        radius = 1,
+                        facecolor = transparent,
+                        edgecolor = edgecolor)
+    ax.add_artist(circle)
+    ax.plot(*point, marker = 'o',
+            color = 'black', markersize = 3)
+    ax.text(*point, f'${label}$',
+            va = 'bottom', ha = 'left')
 
-# Add point c
-angle = math.asin(0.5)
-c1 = math.cos(angle)
-c2 = math.sin(angle)
-ax.plot([c1],[c2], marker = 'o', markersize = 3)
-ax.text(c1,c2,'$c$', va = 'bottom', ha = 'left')# = 1)
-c = plt.Circle((c1, c2), radius = 1, facecolor = white, edgecolor = grey)
-ax.add_artist(c)
-
-# Where does d go?
-ax.text(1.4,1.4,'$d$?', va = 'bottom', ha = 'left')
+ax.text(1.4, 1.4, '$d$?',
+        va = 'bottom', ha = 'left')
 
 ax.axis('off')
 ax.set_aspect('equal')
-
-v = 2.1
-ax.set_xlim([-1.1,v])
-ax.set_ylim([-1.1,v])
+ax.set_xlim(-1.1, 2.1)
+ax.set_ylim(-1.1, 2.1)
